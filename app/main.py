c23fe863808fe6593834d1dcab9da0eaf508f4c9
viannik <ivan.nikitski@gmail.com>
2025-07-03 22:35:54 +0300

    Fix string formatting in log file name generation

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 def main() -> None:
     while True:
         current_time = datetime.now()
-        file_path = f"app-{current_time.strftime("%H_%M_%S")}.log"
+        file_path = f"app-{current_time.strftime('%H_%M_%S')}.log"
         file_row = current_time.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_path, "w") as file:
             file.write(file_row)
