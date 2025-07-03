b857c5cbcd0f1b8b12bd36b2f7d339573d785f6d
viannik <ivan.nikitski@gmail.com>
2025-07-03 22:29:43 +0300

    Implement logging of current time to a file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        current_time = datetime.now()
+        file_path = f"app-{current_time.strftime("%H_%M_%S")}.log"
+        file_row = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        with open(file_path, "w") as file:
+            file.write(file_row)
+        print(f"{file_row} {file_path}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
